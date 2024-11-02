03fd31591642e398824f78aca01abcef538109f7
Jesus <jesus.cont5648@gmail.com>
2024-11-02 01:30:44 -0500

    Initial commit

---
+++ src/main/java/com/acme/nutrimove/platform/NutriMoveApplication.java
@@ -0,0 +1,13 @@
+package com.acme.nutrimove.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriMoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutriMoveApplication.class, args);
+    }
+
+}
